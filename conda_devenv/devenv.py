2d05c2038fa14b2ac011ae816c029c535e05b6c0
Edison Gustavo Muenz <muenz@esss.com.br>
2016-12-21 20:33:21 -0200

    Handling includes

--- conda_devenv/devenv.py
+++ conda_devenv/devenv.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 
-def render_jinja(content, filename):
+def render_jinja(contents, filename):
     import jinja2
     import sys
 
@@ -12,7 +12,64 @@ def render_jinja(content, filename):
         "sys": sys,
     }
 
-    return jinja2.Template(content).render(**jinja_dict)
+    return jinja2.Template(contents).render(**jinja_dict)
+
+
+def handle_includes(root_yaml):
+    # This is a breadth-first search
+    import yaml
+    queue = {"root": root_yaml}
+    visited = {}
+
+    while queue:
+        filename, yaml_dict = queue.popitem()
+        if filename in visited:
+            continue
+
+        for included_filename in yaml_dict.get("includes", []):
+            with open(included_filename, "r") as f:
+                jinja_contents = render_jinja(f.read(), included_filename)
+            included_yaml_dict = yaml.load(jinja_contents)
+            queue[included_filename] = included_yaml_dict
+
+        if "includes" in yaml_dict:
+            del yaml_dict["includes"]
+
+        visited[filename] = yaml_dict
+
+    return visited.values()
+
+
+def merge(dicts, keys_to_skip=('name',)):
+    final_dict = {}
+
+    for d in dicts:
+        for key, value in d.items():
+            if key in final_dict:
+                if key in keys_to_skip:
+                    continue
+                if isinstance(value, list):
+                    final_dict[key].extend(value)
+                else:
+                    message = "Can't merge the key: '{key}' because it will override the previous value. " \
+                              "Only lists can be merged. The type obtained was: {type}".format(key=key,
+                                                                                               type=type(value))
+                    raise ValueError(message)
+            else:
+                final_dict[key] = value
+    return final_dict
+
+
+def render(filename):
+    with open(filename, "r") as f:
+        contents = f.read()
+    rendered_contents = render_jinja(contents, filename)
+
+    import yaml
+    root_yaml = yaml.load(rendered_contents)
+    all_yaml_dicts = handle_includes(root_yaml)
+    final_dict = merge(all_yaml_dicts)
+    return yaml.dump(final_dict)
 
 
 def main():
@@ -31,11 +88,7 @@ def main():
 
     filename = args.filename or "environment.devenv.yml"
 
-    # Render jinja
-    with open(filename, "r") as f:
-        contents = f.read()
-
-    rendered_contents = render_jinja(contents, filename)
+    rendered_contents = render(filename)
 
     if args.print:
         print(rendered_contents)
@@ -48,6 +101,8 @@ def main():
 
     with open(output_filename, 'w') as f:
         f.write(rendered_contents)
+
+    # TODO: call conda env update at the generated file
 
 
 if __name__ == "__main__":
---
+++ conda_devenv/tests/test_include.py
@@ -0,0 +1,85 @@
+import yaml
+
+from conda_devenv.devenv import handle_includes, render_jinja
+
+
+def obtain_yaml_dicts(root_yaml_filename):
+    contents = open(root_yaml_filename, "r").read()
+    contents = render_jinja(contents, filename=root_yaml_filename)
+    root_yaml = yaml.load(contents)
+    dicts = handle_includes(root_yaml)
+    dicts = list(dicts)
+
+    # The list order does not matter, so we can"t use indices to fetch each item
+    dicts = {d["name"]: d for d in dicts}
+    return dicts
+
+
+def test_include(datadir):
+    datadir["a.yml"]
+    datadir["b.yml"]
+    datadir["c.yml"]
+
+    dicts = obtain_yaml_dicts(datadir["c.yml"])
+    assert len(dicts) == 3
+
+    assert dicts["a"] == {
+        "name": "a",
+        "dependencies": [
+            "a_dependency",
+            {"pip": [
+                "a_pip_dependency"
+            ]}
+        ],
+        "environment": [
+            {"PATH": [
+                "a_path"
+            ]}
+        ]
+    }
+
+    assert dicts["b"] == {
+        "name": "b",
+        "dependencies": [
+            "b_dependency",
+            {"pip": [
+                "b_pip_dependency"
+            ]}
+        ],
+        "environment": [
+            {"PATH": [
+                "b_path"
+            ]}
+        ],
+        "channels": [
+            "b_channel",
+        ],
+    }
+
+    assert dicts["c"] == {
+        "name": "c",
+        "channels": [
+            "c_channel",
+        ],
+    }
+
+
+def test_include_non_dag(datadir):
+    datadir["a_non_dag.yml"]
+    datadir["b_non_dag.yml"]
+
+    dicts = obtain_yaml_dicts(datadir["b_non_dag.yml"])
+
+    assert dicts["a"] == {
+        "name": "a",
+        "dependencies": [
+            "a_dependency",
+        ],
+    }
+
+    assert dicts["b"] == {
+        "name": "b",
+        "dependencies": [
+            "b_dependency",
+        ],
+    }
