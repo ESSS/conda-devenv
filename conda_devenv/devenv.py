2985b2f75c605b82c08168d3e353d4222108facc
Edison Gustavo Muenz <muenz@esss.com.br>
2016-12-22 13:10:19 -0200

    Render the environment.yml file correctly so conda-env can use it

--- conda_devenv/devenv.py
+++ conda_devenv/devenv.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+import sys
+
 
 def render_jinja(contents, filename):
     import jinja2
@@ -45,62 +47,110 @@ def merge(dicts, keys_to_skip=('name',)):
 
     for d in dicts:
         for key, value in d.items():
+            if key in keys_to_skip:
+                continue
+
             if key in final_dict:
-                if key in keys_to_skip:
-                    continue
                 if isinstance(value, dict):
                     final_dict[key] = merge([final_dict[key], value])
                 elif isinstance(value, list):
                     final_dict[key].extend(value)
                 else:
                     message = "Can't merge the key: '{key}' because it will override the previous value. " \
-                              "Only lists can be merged. The type obtained was: {type}".format(key=key,
-                                                                                               type=type(value))
+                              "Only lists and dicts can be merged. The type obtained was: {type}"\
+                        .format(
+                            key=key,
+                            type=type(value)
+                        )
                     raise ValueError(message)
             else:
                 final_dict[key] = value
     return final_dict
 
 
-def render(filename):
+def load_yaml_dict(filename):
     with open(filename, "r") as f:
         contents = f.read()
     rendered_contents = render_jinja(contents, filename)
 
     import yaml
     root_yaml = yaml.load(rendered_contents)
+
     all_yaml_dicts = handle_includes(root_yaml)
-    final_dict = merge(all_yaml_dicts)
-    return yaml.dump(final_dict)
+    merged_dict = merge(all_yaml_dicts)
+
+    # Force the "name" because we want to keep the name of the root yaml
+    merged_dict["name"] = root_yaml["name"]
+
+    environment = merged_dict.pop("environment", {})
+    return merged_dict, environment
+
+
+def render_for_conda_env(yaml_dict):
+    import yaml
+    return yaml.dump(yaml_dict, default_flow_style=False)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Work with multiple conda-environment-like yaml files in dev mode.")
-    parser.add_argument("filename", nargs="?",
+    parser.add_argument("--file", "-f", nargs="?",
                         help="The environment.devenv.yml file to process.")
+
+    parser.add_argument("--name", "-n", nargs="?",
+                        help="Name of environment.")
 
     parser.add_argument("--print", help="Only prints the rendered file to stdout and exits.",
                         action="store_true")
 
+    parser.add_argument("--no-prune", help="Don't pass --prune flag to conda-env.",
+                        action="store_true")
+
+    parser.add_argument("--output-file", help="Output filename.",
+                        default="environment.yml")
+
     args = parser.parse_args()
 
-    filename = args.filename or "environment.devenv.yml"
+    filename = args.file or "environment.devenv.yml"
 
-    rendered_contents = render(filename)
+    conda_yaml_dict, environment = load_yaml_dict(filename)
+    rendered_contents = render_for_conda_env(conda_yaml_dict)
 
     if args.print:
         print(rendered_contents)
         return
 
     # Write to the output file
-    output_filename = filename.rstrip(".devenv.yml") + ".yml"
-    if output_filename.endswith(".yml.yml"):
-        raise ValueError("Can't guess the output file, please provide the output file with the --output-filename flag")
+    if args.output_file:
+        output_filename = args.output_file
+    else:
+        output_filename = filename.rstrip(".devenv.yml") + ".yml"
+        if output_filename.endswith(".yml.yml"):
+            raise ValueError("Can't guess the output file, please provide the output file with the --output-filename "
+                             "flag")
 
     with open(output_filename, 'w') as f:
         f.write(rendered_contents)
 
-    # TODO: call conda env update at the generated file
+    # Call conda-env update
+    import subprocess
+    command = [
+        "conda",
+        "env",
+        "update",
+        "--file",
+        output_filename,
+    ]
+    if not args.no_prune:
+        command.append("--prune")
+    if args.name:
+        command.extend(["--name", args.name])
+
+    print("> Executing: %s" % ' '.join(command))
+    retcode = subprocess.call(command)
+    if retcode != 0:
+        sys.exit(retcode)
+
+    # TODO: Write the scripts to set/unset the environment variables
 
 
 if __name__ == "__main__":
