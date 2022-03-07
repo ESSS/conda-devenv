a3d1e099e52f5cba28d5c2f78f9415bcdb23eb5b
Martin Prüsse <prusse.martin@gmail.com>
2022-03-07 08:15:25 -0300

    Add mypy pre-commit hook (#136)
    * Add mypy pre-commit hook
    
    * Change precommit default python version to 3.7
    
    * Remove python 3.6 from build matrix
    
    * Add types-PyYAML to mypy hook requiments
    
    * Fix type hints
    
    * Update setup.py (remove python 3.6)

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 
 import sys
 import os
+from typing import Dict
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
@@ -193,7 +194,7 @@ htmlhelp_basename = "conda_devenvdoc"
 
 # -- Options for LaTeX output ------------------------------------------
 
-latex_elements = {
+latex_elements: Dict[str, str] = {
     # The paper size ('letterpaper' or 'a4paper').
     #'papersize': 'letterpaper',
     # The font size ('10pt', '11pt' or '12pt').
